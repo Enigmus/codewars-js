ea9d891104c6831a190b61234b7b1e40e1141c21
sd <sd_net@mail.ru>
2023-05-06 17:36:19 +0300

    Add comments

--- 5-kyu/Simple-Pig-Latin.js
+++ 5-kyu/Simple-Pig-Latin.js
@@ -1,17 +1,29 @@
-function pigIt(str){
-    let arrFromStr = str.split(' ');
+/**
+ *   @author SD "https://github.com/Enigmus"
+ *   @file Задание из 5 kyu
+ *   Переместите первую букву каждого слова в конец,
+ *   затем добавьте «ay» в конец слова.
+ *   Оставьте знаки препинания нетронутыми.
+ */
+
+/**
+ * @param {string} str - исходная строка
+ * @returns {string} -  Возвращаем  изменненную строку, где первый символ
+ *                      каждого слова перенесен в конец слова, + добавленые символы 'ay'
+ */
+function pigIt(str) {
+    let arrFromStr = str.split(" "); //Разбиваем строку на массив слов
     const newArr = arrFromStr.map((el) => {
-        if(el.length === 1 && (/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g.test(el))) 
+        if (el.length === 1 && /[.,\/#!?$%\^&\*;:{}=\-_`~()]/g.test(el))
+            //если элемент массива знак препинания, возвращем его без изменений
             return el;
-        let arr = el.split('');
-        let letter = arr.shift();
-        arr.push(letter,'ay');
-        return el = arr.join('');
+        let arr = el.split(""); //разбиваем слово на буквы
+        let letter = arr.shift(); //отрезаем первую букву
+        arr.push(letter, "ay"); //доваляем первую букву в конец слова, + два дополнительных символа
+        return (el = arr.join("")); //возвращаем склеенное слово
     });
-    return newArr.join(' ');
-  }
+    return newArr.join(" "); //возвращаем склеенное предложение
+}
 
-
-
-console.log(pigIt('Pig latin is cool'));     // igPay atinlay siay oolcay
-console.log(pigIt('Hello world !'));         // elloHay orldway !
+console.log(pigIt("Pig latin is cool")); // igPay atinlay siay oolcay
+console.log(pigIt("Hello world !")); // elloHay orldway !
--- 6-kyu/Last-digits-of-a-number.js
+++ 6-kyu/Last-digits-of-a-number.js
@@ -1,8 +1,20 @@
-function lastDigit(n, d) {
-    if (d <= 0) 
-        return [];
+/**
+ * @author SD "https://github.com/Enigmus"
+ * @file Задача из 6 kyu
+ * Ваша задача — реализовать функцию, которая возвращает последние D цифры целого числа N в виде списка.
+ */
 
-    return n.toString().slice(-d).split('').map(Number);
+/**
+ *
+ * @param {number} n Исходное число
+ * @param {number} d количество цифр числа D
+ * @returns {Array} массив цифр
+ */
+function lastDigit(n, d) {
+    if (d <= 0) return []; //Если d отрицательное, либо равно нулю, вернем пустой массив
+    // Приводим число к строке, возвращаем последнее D-количество символов, 
+    //разибваем строку на массив, и приводим каждый элемент массива к числу
+    return n.toString().slice(-d).split("").map(Number);
 }
 
 console.log(lastDigit(123767, -4));
