8afac626c9ca38f6f55c09015841010946454502
sd <sd_net@mail.ru>
2023-05-30 22:57:10 +0300

    add

---
+++ Training-JS/#39-Regular-Expression--.js
@@ -0,0 +1,13 @@
+var regex=/^-?9\d*0{4,}$/
+
+console.log(regex.test("90000"));    //true
+console.log(regex.test("-90000"));   //true
+console.log(regex.test("900000000"));    //true
+console.log(regex.test("91230000")); //true
+console.log(regex.test("-91230000"));    //true
+console.log(regex.test("190000"));   //false
+console.log(regex.test("90001"));    //false
+console.log(regex.test("9000")); //false
+console.log(regex.test("91230"));    //false
+console.log(regex.test("1-90000"));  //false
+console.log(regex.test("-90000123"));    //false
