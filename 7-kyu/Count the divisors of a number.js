66931d5b8f886c214ee22d7f1741bb2d14711f81
sd <sd_net@mail.ru>
2023-06-25 21:30:14 +0300

    add

--- 7-kyu/Count the divisors of a number.js
+++ 7-kyu/Count the divisors of a number.js
@@ -1,15 +1,26 @@
-function getDivisorsCnt(n){
-    let c = 0;
-  for(let i = 1; i <= n; i++){
-    if(n % i === 0)
-      c++
-  }
-  return c
+function getDivisorsCnt(n) {
+    let num = n;
+    let count = 1;
+
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            let pow = 1;
+            let ipow = i;
+
+            for (let ipowNext = i * i; num % ipowNext === 0; ipowNext *= i) {
+                pow++;
+                ipow = ipowNext;
+            }
+
+            count *= pow + 1;
+            num = num / ipow;
+        }
+    }
+
+    return count;
 }
 
-
-
-console.log(getDivisorsCnt(1),); // 1
+console.log(getDivisorsCnt(1)); // 1
 console.log(getDivisorsCnt(10)); // 4
 console.log(getDivisorsCnt(11)); // 2
 console.log(getDivisorsCnt(54)); // 8
