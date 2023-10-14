e06db50dea7b48b48a79e671ccfc2130572be2e4
sd <sd_net@mail.ru>
2023-10-14 22:17:32 +0300

    added a new solution :(

--- 4-kyu/Explosive-Sum.js
+++ 4-kyu/Explosive-Sum.js
@@ -1,11 +1,10 @@
 /* Скорость медленная, решение ни проходит на КодВарс :( */
-function sum(num) {
+/* function sum(num) {
     let count = 0;
 
     function partition(num, limit) {
         if (num > 0) {
-            let k = Math.min(num, limit);
-            for (let i = k; i > 0; i--) {
+            for (let i = Math.min(num, limit); i > 0; i--) {
                 partition(num - i, i);
             }
         } else {
@@ -15,6 +14,39 @@ function sum(num) {
     }
 
     return partition(num, num);
+} */
+
+/** Слишком долго просчитывается :( */
+function sum(num){
+    if(num === 1) return 1;
+    let temp = [];  //исходный массива
+    let c = 0;
+    for(let i = 0; i < num; i++)
+        temp.push(1);   //заполняем его единицами. Размерность равна num
+    //console.log(temp);
+
+    do{ //выполняем цикл, пока в первом элементе не будет значения равного num        
+        let min = 0;
+        let s = 0;
+
+        for(let i = 0;i < temp.length - 1; i++){            
+            if(temp[min] > temp[i]){
+                min = i;                    
+            }             
+        }
+        temp[min]++;
+        temp[temp.length - 1]--; 
+
+        for(let i = 0; i < min + 1; i++)
+            s+=temp[i]
+        temp.splice(min + 1);
+        for(let j = 0; j < num - s; j++)
+            temp.push(1)        
+        
+        //console.log(temp); 
+        c++;
+    }while(temp[0] < num)
+    return ++c;
 }
 
 console.log(sum(1)); //1
@@ -22,4 +54,4 @@ console.log(sum(2)); //2
 console.log(sum(3)); //3
 console.log(sum(4)); //5
 console.log(sum(5)); //7
-console.log(sum(10)); //42
+console.log(sum(10)); //42 
