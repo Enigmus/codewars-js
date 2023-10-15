8cd40b5cfc59e7a039644b6f6875a0235a4c51e7
sd <sd_net@mail.ru>
2023-10-15 15:29:15 +0300

    add solution

---
+++ 7-kyu/List-Filtering.js
@@ -0,0 +1,10 @@
+/*В этом ката вы создадите функцию, которая принимает список неотрицательных целых чисел 
+и строк и возвращает новый список с отфильтрованными строками.*/
+
+function filter_list(l) {
+    return l.filter((el) => typeof el == "number");
+}
+
+console.log(filter_list([1, 2, "a", "b"])); //[1,2], 'For input [1,2,"a","b"]'
+console.log(filter_list([1, "a", "b", 0, 15])); //[1,0,15], 'For input [1,"a","b",0,15]'
+console.log(filter_list([1, 2, "aasf", "1", "123", 123])); //[1,2,123], 'For input [1,2,"aasf","1","123",123]'
