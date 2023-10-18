ea7ca373f68f5483a67d644a6eab11af628d1a97
sd <sd_net@mail.ru>
2023-10-18 18:27:57 +0300

    add solution

---
+++ 4-kyu/Next smaller number with the same digits.js
@@ -0,0 +1,19 @@
+/**
+ * Напишите функцию, которая принимает положительное целое число
+ * и возвращает следующее меньшее положительное целое число,
+ *  содержащее те же цифры.
+ */
+
+function nextSmaller(n,i = 0) {
+    if (n < 10) return -1;
+    
+    return num;
+}
+
+//console.log(nextSmaller(21)); //12
+//console.log(nextSmaller(907)); //790
+//console.log(nextSmaller(531)); //513
+//console.log(nextSmaller(135)); //-1
+console.log(nextSmaller(2071)); //2017
+//console.log(nextSmaller(1027)); //-1
+//console.log(nextSmaller(414)); //144 
