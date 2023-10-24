510c127355956a6cc1050f3f41646734490bf127
sd <sd_net@mail.ru>
2023-10-24 21:09:18 +0300

    add valid solution

--- 7-kyu/Power of two.js
+++ 7-kyu/Power of two.js
@@ -1,10 +1,7 @@
-function isPowerOfTwo(n){
-    if(n > 0){
-    if( n % 2 ) return false; else true
-    return isPowerOfTwo(n / 2)
-    }
-  }
+function isPowerOfTwo(n) {
+  return n != 0 && (n & (n - 1)) == 0 ? true : false;
+}
 
-console.log(isPowerOfTwo(2));   //true
-console.log(isPowerOfTwo(4096));    //true
-console.log(isPowerOfTwo(5));   //false
+console.log(isPowerOfTwo(2)); //true
+console.log(isPowerOfTwo(4096)); //true
+console.log(isPowerOfTwo(5)); //false
