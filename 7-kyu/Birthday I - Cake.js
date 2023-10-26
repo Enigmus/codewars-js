18fd3a4e57812c62421ca8c7d563c9c7a4bd0c9e
sd <sd_net@mail.ru>
2023-10-26 23:27:23 +0300

    refactor: removed unnecessary variables

--- 7-kyu/Birthday I - Cake.js
+++ 7-kyu/Birthday I - Cake.js
@@ -1,13 +1,9 @@
 function cake(x, y) {
     let debris = 0;
     for (let i = 0; i < y.length; i += 1) {
-        if (!(i % 2)) debris += y.charCodeAt(i);
-        else debris += y.charCodeAt(i) - 96;
+        debris += (!(i % 2)) ? y.charCodeAt(i) : y.charCodeAt(i) - 96;
     }
-
-    let percent = x * 0.7;
-
-    return percent < debris ? "Fire!" : "That was close!";
+    return x * 0.7 < debris ? "Fire!" : "That was close!";
 }
 
 console.log(cake(900, "abcdef")); //'That was close!
