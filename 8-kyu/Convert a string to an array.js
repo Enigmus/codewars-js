71f0c26e4b9992a21374a3f8a9ecac731af1d4af
sd <sd_net@mail.ru>
2024-12-16 23:06:23 +0300

    init: add solution

---
+++ 8-kyu/Convert a string to an array.js
@@ -0,0 +1,9 @@
+/**
+ * Напишите функцию для разбиения строки и преобразования ее в массив слов.
+ *
+ * @param {String} string
+ * @returns Array
+ */
+function stringToArray(string) {
+  return string.split(" ");
+}
