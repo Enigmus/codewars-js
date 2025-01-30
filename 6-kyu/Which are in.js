c250c41e00b1168a518c94c3565f265336311d6d
sd <sd_net@mail.ru>
2025-01-30 16:55:23 +0300

    более компактное решение

--- 6-kyu/Which are in.js
+++ 6-kyu/Which are in.js
@@ -1,15 +1,12 @@
-function inArray(array1, array2) {
-  return array1
-    .filter((el) => {
-      return array2.find((e) => {
-        return e.search(new RegExp(`${el}`, "g")) !== -1;
-      });
-    })
+const inArray = (array1, array2) =>
+  array1
+    .filter((el) =>
+      array2.find((e) => e.search(new RegExp(`${el}`, "g")) !== -1)
+    )
     .sort((a, b) => a.localeCompare(b));
-}
 
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
 
 a1 = ["live", "strong", "arp"];
 
-console.log(inArray(a1, a2), ["live", "strong"]);
+console.log(inArray(a1, a2), ["arp", "live", "strong"]);
