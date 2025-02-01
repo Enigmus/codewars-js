6db7348b8a5984611ba7be037e724fd378d736f4
sd <sd_net@mail.ru>
2025-02-01 21:15:53 +0300

    Использование метода toSliced()

--- 7-kyu/Complete The Pattern #7 - Cyclical Permutation.js
+++ 7-kyu/Complete The Pattern #7 - Cyclical Permutation.js
@@ -1,12 +1,10 @@
 function pattern(n) {
   if (n < 1) return "";
-  let temp = Array.from({ length: n });
-  return temp
+  return Array.from({ length: n })
     .map((el, i) => {
       let t = Array.from({ length: n }, (_, i) => i + 1);
       let slice = t.slice(0, i);
-      t.splice(0, i);
-      return (i === 0 ? "" : "\n") + t.concat(slice).join("");
+      return (i === 0 ? "" : "\n") + t.toSpliced(0, i).concat(slice).join("");
     })
     .join("");
 }
