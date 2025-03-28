ec95668752925186413d3f042f9ca4d68b0b4044
Сергей Демуров <SD_net@mail.ru>
2025-03-28 21:55:26 +0300

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.js
+++ 7-kyu/vowel-count/vowel-count.js
@@ -1,1 +1,2 @@
-const getCount = (str) => str.split('').reduce((acc,el)=>(['a', 'e', 'i', 'o', 'u'].includes(el)) ? acc + 1 : acc,0);
+const getCount = (str) =>
+  str.split('').reduce((acc,el)=>(['a', 'e', 'i', 'o', 'u'].includes(el)) ? acc + 1 : acc,0);
